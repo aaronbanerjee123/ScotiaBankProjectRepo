6db4cc2d2da7b03f4a81b02c9d80a0d2bb50fed3
aaronbanerjee123 <aaronbaner@outlook.com>
2024-03-23 16:57:06 -0700

    latets

--- public/index.php
+++ public/index.php
@@ -1,14 +1,20 @@
 
 <?php 
-    // error_reporting(0);
+    error_reporting(0);
     session_start();
     require "../app/core/init.php";
     $url = $_GET['url'] ?? 'home';
     $url = strtolower($url);
     $url = explode("/", $url);
 
+    try{
+        print_r($url);
+    }catch(Exception $e){
+        echo $e->getMessage();
+    }
+
     $page_name = trim($url[0]);
-    print_r($url);
+    // print_r($url);
    
     $filename = "../app/pages/".$page_name.".php"; //we get the first part ofthe url for example /public/admin
         //admin is url[0], then we locate it in the pages and add .php to the end 
